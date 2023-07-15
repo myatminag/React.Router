2b4abd3419fafc9a845a623b2f409ffec8faf596
MyatMinAung <myatminaung.dev@gmail.com>
2023-07-15 23:44:15 +0630

    add: vans list

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -9,9 +9,28 @@ const Vans = () => {
             .then((data) => setVans(data.vans));
     }, []);
 
-    console.log("vans", vans);
-
-    return <div>Enter</div>;
+    return (
+        <div className="van-list-container">
+            <h1>Explore our van options</h1>
+            <div className="van-list">
+                {vans.map((van) => (
+                    <div key={van.id} className="van-tile">
+                        <img src={van.imageUrl} />
+                        <div className="van-info">
+                            <h3>{van.name}</h3>
+                            <p>
+                                ${van.price}
+                                <span>/day</span>
+                            </p>
+                        </div>
+                        <i className={`van-type ${van.type} selected`}>
+                            {van.type}
+                        </i>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default Vans;
